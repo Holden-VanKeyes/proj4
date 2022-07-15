bd67dc89b9176e3dffe060c0cff0fa96a94d0f9c
Holden-VanKeyes <102389369+Holden-VanKeyes@users.noreply.github.com>
2022-07-15 11:36:56 -0500

    NavBar fixes

--- project-backend/db/migrate/20220713204908_remove_power_from_items.rb
+++
@@ -1,6 +0,0 @@
-class RemovePowerFromItems < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :items, :power_level, :integer
-    remove_column :items, :power, :integer
-  end
-end
--- project-backend/db/schema.rb
+++ project-backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_13_204908) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_13_203452) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_13_204908) do
     t.string "name"
     t.string "category"
     t.string "image_url"
+    t.integer "power_level"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "stat"
